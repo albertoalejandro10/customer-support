6851955b0adc1ee89a0022f36a1e20d297b2e4ac
AlbertoAlejandro10 <alberto196g@gmail.com>
2022-02-22 00:40:57 -0400

    added bootstrap-select to service.html

--- src/js/service.js
+++ src/js/service.js
@@ -1,15 +1,12 @@
 import * as bootstrap from 'bootstrap'
-import * as selectpicker from 'bootstrap-select'
-
 
 // Conseguir parametros del URL
-export const getParameter = parameterName => {
+const getParameter = parameterName => {
     let parameters = new URLSearchParams( window.location.search )
     return parameters.get( parameterName )
 }
 // console.log(Number(getParameter('id')))
 // console.log(getParameter('name'))
-
 
 // Formatear input cantidad - Es copiado.
 class CampoNumerico {
@@ -60,30 +57,9 @@ class CampoNumerico {
     }
 }
 
-new CampoNumerico('#quantity');
+new CampoNumerico('#quantity')
 
-
-// Fetch para traer datos de productos (detalle y id)
-fetch(`https://62048c21c6d8b20017dc3571.mockapi.io/api/v1/productos`)
-    .then( resp => resp.json() )
-    .then( resp => {
-        const products = resp
-        for ( let element of products ) {
-            // Desestructuracion del objeto element
-            const { detalle, id } = element
-            // console.log(detalle, Number(id))
-
-            let select = document.querySelector('#product')
-            let option = document.createElement("option")
-            option.setAttribute("data-tokens", detalle)
-            option.setAttribute("data-content", detalle)
-            option.value = id
-            option.textContent = detalle
-
-            select.appendChild( option )
-        }
-    })
-
+// Funcion fetch para traer servicios, si viene id en la URL
 const servicePromise = id => {
     fetch(`https://62048c21c6d8b20017dc3571.mockapi.io/api/v1/customers/${id}/Services`)
         .then( resp => resp.json( ))
@@ -215,3 +191,62 @@ if ( getParameter('id') && ! getParameter('name') ) {
     addButton.disabled = true
     servicePromise(getParameter('id'))
 }
+
+
+
+// Method DELETE
+const deleteService = document.getElementById('deleteService')
+deleteService.onclick = () => {
+    const id = getParameter('id')
+    fetch(`https://62048c21c6d8b20017dc3571.mockapi.io/api/v1/customers/${id}/Services/${id}`, {
+        method: 'DELETE',
+    })
+    .then( resp => resp.json() )
+    .then( resp => {
+        console.log(resp)
+        alert('Información eliminada exitosamente')
+        location.reload()
+    })
+    .catch( err => {
+        console.log( err )
+    })
+
+    deleteService.disabled = 'true'
+}
+
+// Metodo POST
+const $form = document.querySelector('#form')
+$form.addEventListener('submit', event => {
+    event.preventDefault()
+    const formData = new FormData(event.currentTarget)
+
+    const id = getParameter('id')
+    const clienteid = getParameter('id')
+    const codigoServ = `nombre ${id}`
+    const nombreServ = formData.get('description')
+    const precioServ = Number(formData.get('netPrice'))
+    const cantidad = Number(formData.get('quantity'))
+    const vencimiento = formData.get('expiration')
+    const tipo = false
+    const activo = false
+    const observacion = formData.get('observation')
+
+    const data = { codigoServ, observacion, nombreServ, cantidad, tipo, precioServ, vencimiento, activo, id, clienteid }
+
+    fetch(`https://62048c21c6d8b20017dc3571.mockapi.io/api/v1/customers/${id}/Services`, {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then( resp => resp.json() )
+    .then( resp => {
+        console.log(resp)
+        alert('Información cargada exitosamente')
+        location.reload()
+    })
+    .catch( err => {
+        console.log( err )
+    })
+})
