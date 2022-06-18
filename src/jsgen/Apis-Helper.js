72b8fa91599c66e9691f4dd9a55332bc1d33e3eb
AlbertoAlejandro10 <alberto196g@gmail.com>
2022-06-18 01:25:32 -0400

    Change wrong URLAPI

--- src/jsgen/Apis-Helper.js
+++ src/jsgen/Apis-Helper.js
@@ -85,6 +85,92 @@ export const get_customers = tkn => {
     })
 }
 
+//Nuevo Listado Productos
+export const get_products = tkn => {
+    //get config para el combo de clientes
+    const url_config_cli = 'https://www.solucioneserp.net/session/login_sid'
+    fetch( url_config_cli , {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${tkn}`
+        }
+    })
+    .then( resp => resp.json() )
+    .then( resp => {
+        //variable cantidad de caracteres
+        let cant_character_to_search = 0
+        const configs_resp = resp
+        const { estado, mensaje, usuarioNombre,  ejercicioNombre, ejercicioInicio, ejercicioCierre, empresaNombre, configuracion } = configs_resp
+        //console.log( estado, mensaje, usuarioNombre,  ejercicioNombre, ejercicioInicio, ejercicioCierre, empresaNombre )
+
+        const config_params = configuracion
+        //const { codigo , valor } = [config_params]
+        
+        for ( const config_ele of config_params ) {        
+            // Desestructuracion del objeto element
+            const { codigo, valor } = config_ele   
+            if ( codigo == 'COMBOTIPOCLIENTES' ) {
+                cant_character_to_search = valor
+            }
+        }   
+
+        let combo_configs = {
+            language: {
+                noResults: function() {    
+                    return "No hay resultado"
+                },
+                searching: function() {
+                    return "Buscando.."
+                },
+                inputTooShort:function() {
+                    return "Ingrese 3 caracteres o mas para buscar"
+                }
+            },
+            placeholder: 'Buscar producto',            
+            ajax: {
+                delay: 500,
+                url: 'https://www.solucioneserp.net/listados/get_productos_filtro',
+                headers: {'Authorization' : 'Bearer ' + tkn},
+                type: 'POST',
+                dataType:'json',
+                data: function (params) {
+                    if ( params.term == null ) {
+                        return JSON.stringify('{filtro:""}')
+                    } else {
+                        return {filtro: params.term}
+                    }
+                },
+                processResults: function (data) {
+                    let arr_t = []
+                    const customers = data
+                    for ( const element of customers ) {
+                        // Desestructuracion del objeto element
+                        const { id, codigo, detalle, unidad, lineaId, iva, activo } = element
+                        arr_t.push({ id: codigo, text: detalle + ' - ' + codigo + ' - ' + unidad })
+                    }
+
+                    return {
+                        //data.items
+                        results: arr_t
+                    }
+                }
+            }
+        }
+
+        if ( cant_character_to_search > 0 ){  
+            combo_configs.minimumInputLength = cant_character_to_search
+        }
+
+        //fin select
+        $(".cmb_productos").select2(combo_configs)
+        // Se usa para que al abrir el combo coloque el foco en el text de busqueda
+        $(".cmb_productos").on('select2:open', function (e) {
+            //alert('test')
+            $(".select2-search__field")[0].focus()
+        })
+    })
+}
+
 // Fecha 01-Mes y Dia actual
 export const get_startMonth = () => {
     const today = new Date().toLocaleDateString('en-GB')
@@ -317,7 +403,7 @@ export const get_deposits = tkn => {
 
 // Listado tipo de comprobantes
 export const get_voucherType = tkn => {
-    const url_getVoucherType = 'https://www.solucioneserp.net/listados/get_depositos'
+    const url_getVoucherType = 'https://www.solucioneserp.net/inventario/reportes/get_tipo_Comprobante'
     fetch( url_getVoucherType, {
         method: 'GET',
         headers: {
