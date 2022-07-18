275f522ef9290f45f7806e5c47b9c8a2338a9783
AlbertoAlejandro10 <alberto196g@gmail.com>
2022-07-18 12:17:00 -0400

    deleting one field on asigncompclientes

--- src/Clientes/js/AsignacionCompClientesGetApis.js
+++ src/Clientes/js/AsignacionCompClientesGetApis.js
@@ -1,89 +1,34 @@
 import { getParameter } from "../../jsgen/Helper"
-import { get_accounts, get_businessUnits, get_startPeriod } from "../../jsgen/Apis-Helper"
+import { get_accounts, get_startPeriod, get_customers } from "../../jsgen/Apis-Helper"
 
-//Nuevo Listado Clientes
-const get_customers = tkn => {
-    //get config para el combo de clientes
-    const url_config_cli = 'https://www.solucioneserp.net/session/login_sid'
-    fetch( url_config_cli , {
+// Listado unidades de negocios
+export const get_businessUnits = tkn => {
+    const url_getBusinessUnits = 'https://www.solucioneserp.net/listados/get_unidades_negocio'
+    fetch( url_getBusinessUnits, {
         method: 'GET',
         headers: {
+            'Content-Type': 'application/json',
             'Authorization': `Bearer ${tkn}`
         }
     })
     .then( resp => resp.json() )
     .then( resp => {
-        //variable cantidad de caracteres
-        let cant_character_to_search = 0
-
-        const configs_resp = resp
-        const { estado, mensaje, usuarioNombre,  ejercicioNombre, ejercicioInicio, ejercicioCierre, empresaNombre, configuracion } = configs_resp
-        //console.log( estado, mensaje, usuarioNombre,  ejercicioNombre, ejercicioInicio, ejercicioCierre, empresaNombre )
-
-        const config_params = configuracion
-        //const { codigo , valor } = [config_params]
-        
-        for ( const config_ele of config_params ) {
-            // Desestructuracion del objeto element
-            const { codigo, valor } = config_ele
-            if ( codigo == 'COMBOTIPOCLIENTES' ) {
-                cant_character_to_search = valor
+        const business = resp
+        for (const element of business) {
+            const { id, nombre } = element
+            // console.log(id, nombre)
+            if ( nombre.toLowerCase() != 'todos') {
+                const select = document.querySelector('#business')
+                let option = document.createElement("option")
+                option.setAttribute("data-tokens", nombre)
+                option.value = id
+                option.textContent = nombre
+                select.appendChild( option )
             }
         }
-
-        let combo_configs = {
-            language: {
-                noResults: function() {
-                    return "No hay resultado"
-                },
-                searching: function() {
-                    return "Buscando.."
-                },
-                inputTooShort: function() {
-                    return "Ingrese 3 caracteres o mas para buscar"
-                }
-            },
-            placeholder: 'Buscar Cliente',
-            ajax: {
-                delay: 500,
-                url: 'https://www.solucioneserp.net/listados/get_clienes_filtro',
-                headers: {'Authorization' : 'Bearer ' + tkn},
-                type: 'POST',
-                dataType:'json',
-                data: function (params) {
-                    if (params.term == null) {
-                        return JSON.stringify('{filtro:""}')
-                    } else {
-                        return {filtro: params.term}
-                    }
-                },
-                processResults: function (data) {
-                    let arr_t = []
-                    const customers = data
-                    for ( const element of customers ) {
-                        // Desestructuracion del objeto element
-                        const { id, codigo, nombre, cuit } = element
-                        arr_t.push({ id: codigo, text: nombre + ' - ' + codigo + ' - ' + cuit })
-                    }
-                    return {
-                        //data.items
-                        results: arr_t
-                    }
-                }
-            }
-        }
-
-        if ( cant_character_to_search > 0 ) {  
-            combo_configs.minimumInputLength = cant_character_to_search
-        }
-
-        $(".cmb_clientes").select2(combo_configs) //fin select
-
-        //se usa para que al abrir el combo coloque el foco en el text de busqueda
-        $(".cmb_clientes").on('select2:open', function (e) {
-            //alert('test');
-            $(".select2-search__field")[0].focus()
-        })
+    })
+    .catch( err => {
+        console.log( err )
     })
 }
 
