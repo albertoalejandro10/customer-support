cf154707ba2d8bc633829f157868d8ffab1f9d34
AlbertoAlejandro <alberto196g@gmail.com>
2023-06-15 12:09:32 -0400

    update labels on NuevoUsuario form

--- src/Inventario/js/IndiceListas.js
+++ src/Inventario/js/IndiceListas.js
@@ -9,30 +9,38 @@ const printIndexesOnTable = (id, nombre, index) => {
   row_data_1.textContent = nombre
   const row_data_2 = document.createElement('td')
   row_data_2.innerHTML = index
+  
+  const buttonInsert = Object.assign(document.createElement('button'), {
+    title: 'Insertar índice',
+    type: 'button',
+    id,
+    name: nombre.trim(),
+    innerHTML: '<i class="fa-solid fa-plus mx-0 mx-0"></i>',
+    className: 'btn btn-primary table-button'
+  })
+  buttonInsert.setAttribute('option', insertOption)
+  buttonInsert.setAttribute('index', index)
 
-  const buttonInsert = document.createElement('button')
-  buttonInsert.title = 'Insertar índice'
-  buttonInsert.type = 'button'
-  buttonInsert.id = id
-  buttonInsert.setAttribute('option', insertOption);
-  buttonInsert.innerHTML = '<i class="fa-solid fa-plus mx-0"></i>'
-  buttonInsert.className = 'btn btn-primary table-button'
+  const buttonEdit = Object.assign(document.createElement('button'), {
+    title: 'Editar índice',
+    type: 'button',
+    id,
+    name: nombre.trim(),
+    innerHTML: '<i class="fa-solid fa-pen-to-square mx-0 mx-0"></i>',
+    className: 'btn btn-success table-button'
+  })
+  buttonEdit.setAttribute('index', index)
   
-  const buttonEdit = document.createElement('button')
-  buttonEdit.title = 'Editar índice'
-  buttonEdit.type = 'button'
-  buttonEdit.id = id
-  buttonEdit.setAttribute('option', editOption);
-  buttonEdit.innerHTML = '<i class="fa-solid fa-pen-to-square mx-0"></i>'
-  buttonEdit.className = 'btn btn-success table-button'
-  
-  const buttonDelete = document.createElement('button')
-  buttonDelete.title = 'Eliminar índice'
-  buttonDelete.type = 'button'
-  buttonDelete.id = id
+  const buttonDelete = Object.assign(document.createElement('button'), {
+    title: 'Eliminar índice',
+    type: 'button',
+    id,
+    name: nombre.trim(),
+    innerHTML: '<i class="fa-solid fa-minus mx-0"></i>',
+    className: 'btn btn-danger table-button'
+  })
   buttonDelete.setAttribute('option', deleteOption)
-  buttonDelete.innerHTML = '<i class="fa-solid fa-minus mx-0"></i>'
-  buttonDelete.className = 'btn btn-danger table-button'
+  buttonDelete.setAttribute('index', index)
   
   const row_data_3 = document.createElement('td')
   row_data_3.appendChild(buttonInsert)
@@ -46,37 +54,56 @@ const printIndexesOnTable = (id, nombre, index) => {
   document.getElementById('full-tbody').appendChild(row)
 }
 
-const get_indexes = tkn => {
-  fetch(process.env.NewSolu_externo + '/inventario/formularios/get_indices', {
+// Declare a global variable to store the fetched data
+let fetchedData;
+
+// Fetch the data and store it in the global variable
+(async () => {
+  const response = await fetch(process.env.NewSolu_externo + '/inventario/formularios/get_indices', {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
       'Authorization': `Bearer ${tkn}`
     }
-  })
-  .then(indexes => indexes.json())
-  .then(({indices: indexes}) => {
-    // console.log(indexes)
-    // Eliminar elementos de la tabla
-    const tableHeaderRowCount = 1
-    const table = document.getElementById('full-table')
-    const rowCount = table.rows.length
-    for (let i = tableHeaderRowCount; i < rowCount; i++) {
-      table.deleteRow(tableHeaderRowCount)
-    }
+  });
+  const data = await response.json();
+  fetchedData = data;
+
+  // Use the fetched data in your code
+  console.log(fetchedData);
+})();
+
+// const get_indexes = tkn => {
+//   fetch(process.env.NewSolu_externo + '/inventario/formularios/get_indices', {
+//     method: 'GET',
+//     headers: {
+//       'Content-Type': 'application/json',
+//       'Authorization': `Bearer ${tkn}`
+//     }
+//   })
+//   .then(indexes => indexes.json())
+//   .then(({indices: indexes}) => {
+//     // console.log(indexes)
+//     // Eliminar elementos de la tabla
+//     const tableHeaderRowCount = 1
+//     const table = document.getElementById('full-table')
+//     const rowCount = table.rows.length
+//     for (let i = tableHeaderRowCount; i < rowCount; i++) {
+//       table.deleteRow(tableHeaderRowCount)
+//     }
     
-    for (const {id, nombre: name, indice: index} of indexes) {
-      printIndexesOnTable(id, name, index)
-    }
-    get_tableButtons()
-  })
-  .catch( error => console.log(error))
-}
+//     for (const {id, nombre: name, indice: index} of indexes) {
+//       printIndexesOnTable(id, name, index)
+//     }
+//     get_tableButtons()
+//   })
+//   .catch( error => console.log(error))
+// }
 
 const tkn = getParameter('tkn')
-if ( tkn ) {
-  get_indexes( tkn )
-}
+// if ( tkn ) {
+//   get_indexes( tkn )
+// }
 
 // Boton actualizar
 document.getElementById('update').addEventListener('click', (event) => {
@@ -89,17 +116,29 @@ const get_tableButtons = () => {
   for (const button of buttons) {
     button.addEventListener("click", event => {
       event.preventDefault()
-      // console.log(event.currentTarget.getAttribute('id'));
-      // console.log(event.currentTarget.getAttribute('option'));
+      const id = event.currentTarget.getAttribute('id')
+      const name = event.currentTarget.getAttribute('name')
+      const index = event.currentTarget.getAttribute('index')
+      console.log(id);
+      console.log(name);
+      console.log(index);
+      // console.log(event.currentTarget.getAttribute('id'))
+      // console.log(event.currentTarget.getAttribute('option'))
       if ( Number(event.currentTarget.getAttribute('option')) === insertOption) {
-        console.log('insert button');
+        // insertIntoApi(id, name, index)
+        return
       }
       if ( Number(event.currentTarget.getAttribute('option')) === editOption) {
-        console.log('edit button');
+        console.log('edit button')
+        return
       }
       if ( Number(event.currentTarget.getAttribute('option')) === deleteOption) {
-        console.log('delete button');
+        deleteIndex(id, name, index)
       }
     })
   }
+}
+
+const deleteIndex = (id, name, index) => {
+  
 }
