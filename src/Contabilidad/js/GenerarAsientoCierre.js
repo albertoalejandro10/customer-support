c12831bb584639c281ed0c50cadcfed2d1f1bf13
AlbertoAlejandro <alberto196g@gmail.com>
2023-07-27 14:40:02 -0400

    finish generarAsientoCierre

--- src/Contabilidad/js/GenerarAsientoCierre.js
+++ src/Contabilidad/js/GenerarAsientoCierre.js
@@ -1,68 +1,93 @@
 import { getParameter } from "../../jsgen/Helper"
 
-// Listado de Ejercicios
-const get_exercises = tkn => {
-    const url_getExercises = process.env.Solu_externo + '/utilidades/ejercicio/get_ejercicios'
-    fetch( url_getExercises, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${tkn}`
-        }
+// Listado plan de cuentas
+const get_accountPlan = tkn => {
+  const url_getAccountPlan = process.env.Solu_externo + '/listados/get_plan_cuenta'
+  fetch( url_getAccountPlan, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${tkn}`
+    },
+    body: JSON.stringify({
+      "tipo": 1,
+      "alfa": 1
     })
-    .then( resp => resp.json() )
-    .then( resp => {
-        const exercises = resp
-        for (const element of exercises) {
-            const { id, ejercicio, inicio, cierre } = element
-            // console.log(id, ejercicio, inicio, cierre)
-            const select = document.querySelector('#exercise')
-            let option = document.createElement("option")
-            option.setAttribute("data-tokens", id)
-            option.value = id
-            option.textContent = ejercicio
-            select.appendChild( option )
-        }
-    })
-    .catch( err => {
-        console.log( err )
-    })
+  })
+  .then( accountPlan => accountPlan.json())
+  .then( accountsPlan => {
+    // console.log(accountsPlan)
+    for (const {codigo, nombre} of accountsPlan) {
+      const select = document.querySelector('#result-account')
+      let option = document.createElement("option")
+      option.setAttribute("data-tokens", codigo)
+      option.value = codigo
+      option.textContent = nombre
+      select.appendChild( option )
+    }
+  })
+  .catch( err => {
+    console.log( err )
+  })
 }
-// Ejecutar
+
 const tkn = getParameter('tkn')
 if ( tkn ) {
-    get_exercises( tkn )
+  get_accountPlan( tkn )
 }
 
-const post_modifiedExercise = (tkn, data) => {
-    const url_postModifiedExercise = process.env.Solu_externo + '/utilidades/ejercicio/modificar_ejercicio_usuario'
-    fetch( url_postModifiedExercise , {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${tkn}`
-        },
-        body: JSON.stringify(data)
-    })
-    .then( resp => resp.json())
-    .then( resp => {
-        document.getElementById('sucess').classList.remove('d-none')
-        document.getElementById('error').classList.add('d-none')
-    })
-    .catch( err => {
-        console.log( err )
-        document.getElementById('error').classList.remove('d-none')
-        document.getElementById('sucess').classList.add('d-none')
-    })
+const post_generateClosure = data => {
+  const url_postGenerateClouse = process.env.Solu_externo + '/contabilidad/asiento/generar_anular_cierre'
+  fetch( url_postGenerateClouse , {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${tkn}`
+      },
+      body: JSON.stringify(data)
+  })
+  .then( resp => resp.json())
+  .then( ({nro, descripcion}) => {
+    alert(`Número: ${nro} \nDescripción: ${descripcion}`)
+    switch(nro) {
+      case -1:
+      case 4073:
+        generateButton.classList.add('d-none')
+        cancelButton.classList.remove('d-none')
+        break
+      case 1:
+        generateButton.classList.remove('d-none')
+        cancelButton.classList.add('d-none')
+        break
+    }
+  })
+  .catch( err => {
+      console.log( err )
+  })
 }
 
-// Boton actualizar
-const $form = document.getElementById('form')
-$form.addEventListener('submit', event => {
-    event.preventDefault()
-    const formData = new FormData(event.currentTarget)
-    const ejercicioId = Number(formData.get('exercise'))
-    const data = { ejercicioId }
-    // console.table( data )
-    post_modifiedExercise( tkn, data )
+const form = document.getElementById('form')
+const handleButtonClick = (event, generarcierre) => {
+  if (!form.reportValidity()) {
+    return;
+  }
+  event.preventDefault()
+  const formData = new FormData(form)
+  const cuenta = formData.get('result-account')
+  const data = {
+    cuenta,
+    generarcierre
+  }
+  // console.log(data)
+  post_generateClosure(data)
+}
+
+const generateButton = document.getElementById('generate-closure')
+const cancelButton = document.getElementById('cancel-closure')
+generateButton.addEventListener('click', event => {
+  handleButtonClick(event, true)
+})
+
+cancelButton.addEventListener('click', event => {
+  handleButtonClick(event, false)
 })
