e00372d50533acf00544b17f7090494961860413
AlbertoAlejandro <alberto196g@gmail.com>
2023-08-24 22:29:39 -0400

    form estadoSituacionPatrimonial

--- src/Contabilidad/js/EstadoSituacionPatrimonial.js
+++ src/Contabilidad/js/EstadoSituacionPatrimonial.js
@@ -2,7 +2,7 @@ import { getParameter, format_number, format_token } from "../../jsgen/Helper"
 
 const tkn = getParameter('tkn')
 const post_getResultState = data => {
-	fetch( process.env.Solu_externo + '/contabilidad/estado_resultado/obtener' , {
+	fetch( process.env.Solu_externo + '/contabilidad/estado_situacion_patrimonial/obtener' , {
 		method: 'POST',
 		body: JSON.stringify(data),
 		headers: {
@@ -11,51 +11,30 @@ const post_getResultState = data => {
 		}
 	})
 	.then( result => result.json() )
-	.then( ({estado_resultado}) => {
-		console.log(estado_resultado)
-		// const incomeTable = document.getElementById('income-tbody')
-		// const expensesTable = document.getElementById('expenses-tbody')
-		// cleanTables(incomeTable)
-		// cleanTables(expensesTable)
+	.then( ({estado_sit_patrimonial}) => {
+		// console.log(estado_sit_patrimonial)
+		const activeTable = document.getElementById('active-tbody')
+		const passiveTable = document.getElementById('passive-tbody')
+		cleanTables(activeTable)
+		cleanTables(passiveTable)
 
-		// const incomeArray = estado_resultado.filter(income => income.ptipo === 'I')
-		// const expensesArray = estado_resultado.filter(expenses => expenses.ptipo === 'G')
+		const activeArray = estado_sit_patrimonial.filter(active => active.ptipo === 'A')
+		const passiveArray = estado_sit_patrimonial.filter(passive => passive.ptipo === 'P')
 
-		// const income = organizeAndFilter(incomeArray)
-		// const expenses = organizeAndFilter(expensesArray)
-		// printTable(incomeTable, income)
-		// printTable(expensesTable, expenses)
+		printTable(activeTable, activeArray)
+		printTable(passiveTable, passiveArray)
 
-		// printFooter(incomeArray, expensesArray)
+		printFooter(activeArray, passiveArray)
 
-		// loadingx.forEach(elem => elem.classList.add('d-none'))
-		// loading.forEach(elem => elem.classList.remove('d-none'))
-		// print.disabled = false
+		loadingx.forEach(elem => elem.classList.add('d-none'))
+		loading.forEach(elem => elem.classList.remove('d-none'))
+		print.disabled = false
 	})
 	.catch( err => {
 		loadingx.forEach(elem => elem.classList.add('d-none'))
 		loading.forEach(elem => elem.classList.add('d-none'))
 		console.log(err)
 	})
-}
-
-const organizeAndFilter = organizeArrangement => {
-	// Organizar arreglos dependiendo de ingresos y egresos
-  const levelOneExpenses = organizeArrangement.filter(item => item.codigo.trim().length === 1)
-  const levelTwoExpenses = organizeArrangement.filter(item => item.codigo.trim().length === 2)
-  const levelFourExpenses = organizeArrangement.filter(item => item.codigo.trim().length === 5)
-
-  levelOneExpenses.sort((a, b) => a.codigo.trim() - b.codigo.trim())
-  levelTwoExpenses.sort((a, b) => a.codigo.trim() - b.codigo.trim())
-  levelFourExpenses.sort((a, b) => a.codigo.trim() - b.codigo.trim())
-
-  const arr = levelTwoExpenses.map(item => levelFourExpenses.filter(subItem => subItem.codigo.startsWith(item.codigo.trim())))
-
-  for (const [index, element] of arr.entries()) {
-    element.unshift(levelTwoExpenses[index])
-	}
-	// console.log(arr)
-  return arr
 }
 
 const cleanTables = table => {
@@ -86,74 +65,66 @@ const printTable = (table, data) => {
 	document.getElementById('empty-data').classList.add('d-none')
 
 	// Iterate over each object in data array
-	data.forEach(item => {
-		for (const { codigo, nombre, saldo_anterior, saldo, linkMayorCuentas } of item) {
-			const row = document.createElement('tr')
+	for (const { codigo, nombre, saldo, linkMayorCuentas } of data) {
+		const row = document.createElement('tr')
 
-			// Create 'td' elements and append to 'tr'
-			const row_data_1 = createAndAppendTd(row, nombre)
-			const row_data_2 = createAndAppendTd(row, format_number(saldo_anterior))
-			const row_data_3 = createAndAppendTd(row, format_number(saldo))
+		// Create 'td' elements and append to 'tr'
+		const row_data_1 = createAndAppendTd(row, nombre)
+		const row_data_2 = createAndAppendTd(row, format_number(saldo))
 
-			// Add specific classes and attributes based on conditions
-			const anchor = document.createElement('a')
-			if (codigo.trim().length === 2) {
-				row_data_1.classList.add('font-weight-bold')
-				row_data_2.classList.add('font-weight-bold')
-				row_data_3.classList.add('font-weight-bold')
-			} else {
-				anchor.setAttribute('href', format_token(linkMayorCuentas))
-				anchor.textContent = nombre
-				row_data_1.textContent = ''
-				row_data_1.appendChild(anchor)
-				row_data_1.classList.add('pl-4')
-			}
-			// Append 'tr' to table
-			table.appendChild(row)
+		// Add specific classes and attributes based on conditions
+		const anchor = document.createElement('a')
+		if (codigo.trim().length === 1) {
+			row_data_1.classList.add('font-weight-bold')
+			row_data_2.classList.add('font-weight-bold')
+		} else if (codigo.trim().length === 2) {
+			row_data_1.classList.add('font-weight-bold')
+			row_data_1.classList.add('pl-3')
+			row_data_2.classList.add('font-weight-bold')
+		} else if (codigo.trim().length === 3) {
+			row_data_1.classList.add('font-weight-bold')
+			row_data_1.classList.add('pl-5')
+			row_data_2.classList.add('font-weight-bold')
+		} else {
+			anchor.setAttribute('href', format_token(linkMayorCuentas))
+			anchor.textContent = nombre
+			row_data_1.textContent = ''
+			row_data_1.appendChild(anchor)
+			row_data_1.classList.add('pl-6')
 		}
-	})
+		// Append 'tr' to table
+		table.appendChild(row)
+	}
 }
 
-const printFooter = (income, expenses) => {
-	if (!income || !income[0] || !expenses || !expenses[0]) {
+const printFooter = (active, passive) => {
+	console.log(active)
+	console.log(passive)
+	if (!active || !active[0] || !passive || !passive[0]) {
 		throw new Error('Invalid input')
 	}
 
 	// Update DOM
-	const updateDOM = (id, value, isTotal) => {
+	const updateFooter = (id, value, isTotal) => {
 		if (isTotal) {
 			return document.getElementById(id).textContent = value !== 0 ? format_number(value) : ''
 		}
 		return document.getElementById(id).textContent = format_number(value)
 	}
 
-	const previousBalanceIncome = Number(income[0].saldo_anterior)
-	const totalBalanceIncome = Number(income[0].saldo_total)
-	const previousBalanceExpenses = Number(expenses[0].saldo_anterior)
-	const totalBalanceExpenses = Number(expenses[0].saldo_total)
+	const totalBalanceActive = Number(active[0].saldo_total)
+	const totalBalancePassive = Number(passive[0].saldo_total)
 
-	updateDOM('total-previous-income', previousBalanceIncome, false)
-	updateDOM('total-previous-expenses', previousBalanceExpenses, false)
-	updateDOM('total-income', totalBalanceIncome, false)
-	updateDOM('total-expenses', totalBalanceExpenses, false)
-
-	// Calculate differences
-	let previousDifference = 0
-	if (previousBalanceIncome >= previousBalanceExpenses) {
-		previousDifference = previousBalanceIncome - previousBalanceExpenses
-		updateDOM('total-income-previous-difference', previousDifference, true)
-	} else if (previousBalanceIncome < previousBalanceExpenses) {
-		previousDifference = previousBalanceIncome - previousBalanceExpenses
-		updateDOM('total-expenses-previous-difference', previousDifference, true)
-	}
+	updateFooter('total-active', totalBalanceActive, false)
+	updateFooter('total-passive', totalBalancePassive, false)
 
 	let totalDifference = 0
-	if (totalBalanceIncome >= totalBalanceExpenses) {
-		totalDifference = totalBalanceIncome - totalBalanceExpenses
-		updateDOM('total-income-difference', totalDifference, true)
-	} else if (totalBalanceIncome < totalBalanceExpenses) {
-		totalDifference = totalBalanceIncome - totalBalanceExpenses
-		updateDOM('total-expenses-difference', totalDifference, true)
+	if (totalBalanceActive >= totalBalancePassive) {
+		totalDifference = totalBalanceActive - totalBalancePassive
+		updateFooter('total-active-difference', totalDifference, true)
+	} else if (totalBalanceActive < totalBalancePassive) {
+		totalDifference = totalBalanceActive - totalBalancePassive
+		updateFooter('total-passive-difference', totalDifference, true)
 	}
 }
 
@@ -185,7 +156,7 @@ const print = document.getElementById('print')
 print.onclick = () => {
 	const data = extractFormData(form)
 
-	let returnURL = window.location.protocol + '//' + window.location.host + process.env.VarURL + '/Contabilidad/VerEstadoResultados.html?'
+	let returnURL = window.location.protocol + '//' + window.location.host + process.env.VarURL + '/Contabilidad/VerEstadoSituacionPatrimonial.html?'
 	for (const property in data) {
 		returnURL += `${property}=${data[property]}&`
 	}
---
+++ src/Contabilidad/js/VerEstadoSituacionPatrimonial.js
@@ -0,0 +1,160 @@
+import { getParameter, format_number, format_token } from "../../jsgen/Helper"
+
+const get_userData = () => {
+	const url_getUserData = process.env.Solu_externo + '/session/login_sid'
+	fetch( url_getUserData , {
+		method: 'GET',
+		headers: {
+			'Content-Type': 'application/json',
+			'Authorization': `Bearer ${tkn}`
+		}
+	})
+	.then( user => user.json() )
+	.then( ({ empresaNombre, empresaCUIT }) => {
+		document.getElementById('name-company').textContent = empresaNombre
+		document.getElementById('cuit-company').textContent = empresaCUIT
+	})
+	.catch( err => {
+		console.log( 'Error en el llamado a la API: ', err )
+	})
+}
+
+const get_dataFromURL = () => {
+	const data = window.location.search.substring(1).split('&').reduce((acc, element) => {
+		const [ property, value ] = element.split('=')
+		acc[property] = value
+		return acc
+	}, {})
+	// console.log(data)
+	printHeader(data.unidad_negocio)
+	post_getResultState(data)
+}
+
+const post_getResultState = data => {
+	document.getElementById('date').textContent = `${data.fecha_desde} - ${data.fecha_hasta}`
+	const url_postGetResultState = process.env.Solu_externo + '/contabilidad/estado_situacion_patrimonial/obtener'
+	fetch( url_postGetResultState , {
+		method: 'POST',
+		body: JSON.stringify(data),
+		headers: {
+			'Content-Type': 'application/json',
+			'Authorization': `Bearer ${tkn}`
+		}
+	})
+	.then( result => result.json() )
+	.then( ({ estado_sit_patrimonial }) => {
+		// console.log(estado_sit_patrimonial)
+		const assetsTable = document.getElementById('assets-tbody')
+		const passiveTable = document.getElementById('passive-tbody')
+
+		const assetsArray = estado_sit_patrimonial.filter(assets => assets.ptipo === 'A')
+		const passiveArray = estado_sit_patrimonial.filter(passive => passive.ptipo === 'P')
+
+		printTable(assetsTable, assetsArray)
+		printTable(passiveTable, passiveArray)
+
+		printFooter(assetsTable, assetsArray)
+		printFooter(passiveTable, passiveArray)
+	})
+	.catch( err => {
+		console.log( err )
+	})
+}
+
+const printHeader = idBusiness => {
+	fetch( process.env.Solu_externo + '/listados/get_unidades_negocio', {
+		method: 'GET',
+		headers: {'Content-Type': 'application/json', 'Authorization': `Bearer ${tkn}`}
+	})
+	.then( business => business.json() )
+	.then( business => {
+		const result = business.filter(x => x.id === Number(idBusiness))
+		if ( result?.length ) {
+			const [{nombre}] = result
+			document.getElementById('business').innerHTML = `<strong class="real-blue">${nombre}</strong>`
+		}
+	})
+}
+
+const tkn = getParameter('tkn')
+if (tkn) {
+	get_userData()
+	get_dataFromURL()
+}
+
+// Helper function to create 'td' and append to 'tr'
+const createAndAppendTd = (parent, content) => {
+	const td = document.createElement('td')
+	td.textContent = content
+	parent.appendChild(td)
+	return td
+}
+
+// Main function to print data into a table
+const printTable = (table, data) => {
+	// Check if table element and data array are valid
+	if (!table) {
+		throw new Error('Table element is not defined')
+	}
+	if (!data || data.length === 0) {
+		throw new Error('Data is not defined or empty')
+	}
+
+	// Iterate over each object in data array
+	for (const { codigo, nombre, saldo, linkMayorCuentas } of data) {
+		const row = document.createElement('tr')
+
+		// Create 'td' elements and append to 'tr'
+		const row_data_1 = createAndAppendTd(row, nombre)
+		const row_data_2 = createAndAppendTd(row, format_number(saldo))
+
+		// Add specific classes and attributes based on conditions
+		const anchor = document.createElement('a')
+		if (codigo.trim().length === 1) {
+			row_data_1.classList.add('font-weight-bold')
+			row_data_2.classList.add('font-weight-bold')
+		} else if (codigo.trim().length === 2) {
+			row_data_1.classList.add('font-weight-bold')
+			row_data_1.classList.add('pl-3')
+			row_data_2.classList.add('font-weight-bold')
+		} else if (codigo.trim().length === 3) {
+			row_data_1.classList.add('font-weight-bold')
+			row_data_1.classList.add('pl-5')
+			row_data_2.classList.add('font-weight-bold')
+		} else {
+			anchor.setAttribute('href', format_token(linkMayorCuentas))
+			anchor.textContent = nombre
+			row_data_1.textContent = ''
+			row_data_1.appendChild(anchor)
+			row_data_1.classList.add('pl-6')
+		}
+		// Append 'tr' to table
+		table.appendChild(row)
+	}
+}
+
+let saldo = 0
+const printFooter = (table, balance) => {
+	saldo += Number(balance[0].saldo)
+	const row = document.createElement('tr')
+	row.classList.add('font-weight-bold')
+	createAndAppendTd(row, balance[0].nombre)
+	createAndAppendTd(row, format_number(balance[0].saldo))
+
+	document.getElementById('balance').textContent = format_number(saldo)
+	table.appendChild(row)
+}
+
+document.getElementById('print').addEventListener('click', () => {
+  try {
+    const printContents = document.querySelector('#printableArea').outerHTML
+    const originalContents = document.body.innerHTML
+    document.body.innerHTML = printContents
+    window.print()
+    setTimeout(function() {
+      document.body.innerHTML = originalContents
+    }, 100) // Add a delay of 100 milliseconds before restoring the original content
+  } catch (error) {
+    console.error('An error occurred while printing:', error)
+  }
+})
