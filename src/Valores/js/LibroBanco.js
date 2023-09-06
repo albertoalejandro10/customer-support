777805317357be26c5c69015ac1030049fb84b7a
AlbertoAlejandro <alberto196g@gmail.com>
2023-09-06 15:09:36 -0400

    form librobanco

---
+++ src/Valores/js/LibroBanco.js
@@ -0,0 +1,239 @@
+import { getParameter, format_number, numbersOnly, format_currency } from "../../jsgen/Helper"
+import { ag_grid_locale_es, comparafecha, dateComparator, filterChangedd } from "../../jsgen/Grid-Helper"
+
+const localeText = ag_grid_locale_es
+const gridOptions = {
+	headerHeight: 28,
+	rowHeight: 24,
+	defaultColDef: {
+		editable: false,
+		resizable: true,  
+		suppressNavigable: true,
+		//minWidth: 100,
+	},
+	// No rows and grid loader
+	overlayLoadingTemplate:
+	'<div class="loadingx" style="margin: 7em"></div>',
+	overlayNoRowsTemplate:
+	'<span class="no-rows"> No hay información </span>',
+
+	onFilterChanged: event => filterChangedd(event),
+	suppressExcelExport: true,
+	popupParent: document.body,
+	localeText: localeText,
+
+	columnDefs: [
+		{
+			width: 85,
+			field: "fecha",
+			sortable: true,
+			filter: true,
+			filter: 'agDateColumnFilter',
+			comparator: dateComparator,
+			filterParams: {
+				// provide comparator function
+				comparator: comparafecha
+			}
+		},
+		{
+			width: 90,
+			field: "fechap",
+			headerName: "Presentación",
+			tooltipField: 'fechap',
+			cellRenderer: function(params) {
+				return params.value
+			}
+		},
+		{
+			width: 100,
+			field: "operacion",
+			headerName: "Operación",
+			tooltipField: 'operacion',
+			cellRenderer: function(params) {
+				return params.value
+			}
+		},
+		{
+			width: 80,
+			field: "numero",
+			headerName: "Número",
+			tooltipField: 'numero',
+			sortable: true,
+			filter: true,
+			cellRenderer: function(params) {
+				return params.value
+			}
+		},
+		{
+			flex: 1,
+			minWidth: 100,
+			field: "detalle",
+			tooltipField: 'detalle',
+			sortable: true,
+			filter: true,
+			cellRenderer: function(params) {
+				return params.value === null ? "Totales" : params.value;
+			}
+		},
+		{
+			width: 100,
+			headerClass: "ag-right-aligned-header",
+			cellClass: 'cell-vertical-align-text-right',
+			field: "importe",
+			sortable: true,
+			filter: true,
+			cellRenderer: function (params) {
+				return params.value === null ? '' : format_number(params.value)
+			}
+		},
+		{
+			width: 100,
+			headerClass: "ag-right-aligned-header",
+			cellClass: 'cell-vertical-align-text-right',
+			field: "saldo",
+			sortable: true,
+			filter: true,
+			cellRenderer: function(params) {
+				return format_number(params.value)
+			}
+		},
+		{
+			width: 100,
+			headerClass: "ag-right-aligned-header",
+			cellClass: 'cell-vertical-align-text-right',
+			field: "consilidado",
+			headerName: "Conciliado",
+			sortable: true,
+			filter: true,
+			cellRenderer: function(params) {
+				return params.value
+			}
+		}
+	],
+	rowData: [],
+	getRowStyle: params => {
+		if (params.node.rowPinned) {
+			return { 'font-weight': 'bold' }
+		}
+	},
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+	const gridDiv = document.querySelector('#myGrid')
+	new agGrid.Grid(gridDiv, gridOptions)
+
+	if ((parseInt($(window).height()) - 300) < 200)
+		$("#myGrid").height(100)
+	else
+		$("#myGrid").height(parseInt($(window).height()) - 295)
+})
+
+function generatePinnedBottomData () {
+	// generate a row-data with null values
+	let result = {}
+	gridOptions.api.columnModel.gridColumns.forEach(item => {
+		result[item.colId] = null
+	})
+	return calculatePinnedBottomData(result)
+}
+
+function calculatePinnedBottomData(target) {
+	let columnsWithAggregation = ['saldo']
+	columnsWithAggregation.forEach(element => {
+		let values = []
+		gridOptions.api.forEachNodeAfterFilter((rowNode) => {
+			if (rowNode.data[element]) {
+				values.push(Number(rowNode.data[element].toFixed(2)))
+			}
+		})
+		if (values.length > 0) {
+			target[element] = values[values.length - 1]
+		}
+	})
+	return target
+}
+
+const get_listDetails = data => {
+	// Mostrar Loader Grid
+	gridOptions.api.showLoadingOverlay()
+	const url_getListDetails = process.env.Solu_externo + '/bancosyvalores/librobanco/listar_detalles'
+	fetch( url_getListDetails , {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+			'Authorization': `Bearer ${tkn}`
+		},
+		body: JSON.stringify(data)
+	})
+	.then( details => details.json() )
+	.then(({ registros }) => {
+		
+		let saldo = 0
+		registros = registros.map(obj => {
+			saldo += obj.importe
+			return {
+				...obj,
+				saldo
+			}
+		})
+
+		// Clear Filtros
+		gridOptions.api.setFilterModel(null)
+		// Clear Grilla
+		gridOptions.api.setRowData([])
+		gridOptions.api.applyTransaction({
+			add: registros
+		})
+		
+		let pinnedBottomData = generatePinnedBottomData()
+		gridOptions.api.setPinnedBottomRowData([pinnedBottomData])
+		
+		gridOptions.api.hideOverlay()
+		document.getElementById('print').disabled = false
+		
+		if ( Object.keys( registros ).length === 0 ) {
+			// console.log( 'Is empty')
+			gridOptions.api.setPinnedBottomRowData([])
+			gridOptions.api.showNoRowsOverlay()
+			document.getElementById('print').disabled = true
+		}
+	})
+	.catch( err => {
+		console.log( err )
+		document.getElementById('print').disabled = true
+	})
+}
+
+const tkn = getParameter('tkn')
+// Boton actualizar
+form.addEventListener('submit', event => {
+	event.preventDefault()
+	const formData = new FormData(event.currentTarget)
+	const data = {
+		unidad_negocio: Number(formData.get('business')),
+		dfecha: formData.get('periodStart').split('-').reverse().join('/'),
+		hfecha: formData.get('periodEnd').split('-').reverse().join('/'),
+		cuenta: Number(formData.get('account')),
+		numero: formData.get('number'),
+		operacion: Number(formData.get('operation')),
+		estado: formData.get('status'),
+		importe: Number(formData.get('net').replaceAll('.', '').replace(',', '.')),
+		detalle: formData.get('detail')
+	}
+	// console.log(data)
+	get_listDetails( data )
+})
+
+const numberElement = document.getElementById('number')
+numberElement.addEventListener('keyup', () => {
+	numberElement.value = numbersOnly(numberElement.value)
+})
+
+$("input[data-type='currency']").on({
+	keyup: function() {
+		format_currency($(this))
+	},
+	blur: function() { 
+		format_currency($(this), "blur")
+	}
+})
--- src/jsgen/Apis-Helper-BancaValores.js
+++ src/jsgen/Apis-Helper-BancaValores.js
@@ -1,144 +1,130 @@
-// Listado de cuentas estado (Valores)
-export const get_statusAccount = tkn => {
-    const url_getStatusAccount = process.env.Solu_externo + '/bancosyvalores/reportes/get_cuentas_estado'
-    fetch( url_getStatusAccount, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${tkn}`
-        }
+// Funcion generica para manejar los fetch requests.
+async function fetchData(url, token) {
+  try {
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+      }
     })
-    .then( resp => resp.json() )
-    .then( resp => {
-        const statusAccounts = resp
-        for (const element of statusAccounts) {
-            const { codigo, nombre } = element
-            // console.log(codigo, nombre)
-            const select = document.querySelector('#status')
-            let option = document.createElement("option")
-            option.setAttribute("data-tokens", nombre)
-            option.value = codigo
-            option.textContent = nombre
-            select.appendChild( option )
-        }
-    })
-    .catch( err => {
-        console.log( err )
-    })
+    
+    if (!response.ok) {
+      throw new Error(`HTTP error: ${response.status}`)
+    }
+    
+    const data = await response.json()
+    return data
+  } catch (error) {
+    console.error(`Fetch error: ${error}`)
+  }
 }
 
-// Listado de tipos de periodo (Valores)
-export const get_periodType = tkn => {
-    const url_getPeriodType = process.env.Solu_externo + '/bancosyvalores/reportes/get_tipos_periodo'
-    fetch( url_getPeriodType, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${tkn}`
-        }
-    })
-    .then( resp => resp.json() )
-    .then( resp => {
-        const periodTypes = resp
-        for (const element of periodTypes) {
-            const { codigo, nombre } = element
-            // console.log(codigo, nombre)
-            const select = document.querySelector('#period-type')
-            let option = document.createElement("option")
-            option.setAttribute("data-tokens", nombre)
-            option.value = codigo
-            option.textContent = nombre
-            select.appendChild( option )
-        }
-    })
-    .catch( err => {
-        console.log( err )
-    })
-}
-// Listado de orden (Valores)
-export const get_order = tkn => {
-    const url_getOrder = process.env.Solu_externo + '/bancosyvalores/reportes/get_tipos_orden'
-    fetch( url_getOrder, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${tkn}`
-        }
-    })
-    .then( resp => resp.json() )
-    .then( resp => {
-        const Orders = resp
-        for (const element of Orders) {
-            const { codigo, nombre } = element
-            // console.log(codigo, nombre)
-            const select = document.querySelector('#order')
-            let option = document.createElement("option")
-            option.setAttribute("data-tokens", nombre)
-            option.value = codigo
-            option.textContent = nombre
-            select.appendChild( option )
-        }
-    })
-    .catch( err => {
-        console.log( err )
-    })
-}
-// Listado de Valor (Valores)
-export const get_value = tkn => {
-    const url_getValue = process.env.Solu_externo + '/bancosyvalores/reportes/get_tipos_valores'
-    fetch( url_getValue, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${tkn}`
-        }
-    })
-    .then( resp => resp.json() )
-    .then( resp => {
-        const values = resp
-        for (const element of values) {
-            const { id, nombre } = element
-            // console.log(id, nombre)
-            const select = document.querySelector('#value')
-            let option = document.createElement("option")
-            option.setAttribute("data-tokens", nombre)
-            option.value = id
-            option.textContent = nombre
-            select.appendChild( option )
-        }
-    })
-    .catch( err => {
-        console.log( err )
-    })
+// Listado de cuentas estado (Formulario Valores)
+export const get_statusAccount = async tkn => {
+  const url_getStatusAccount = process.env.Solu_externo + '/bancosyvalores/reportes/get_cuentas_estado'
+  const statusAccounts = await fetchData(url_getStatusAccount, tkn)
+
+  for (const { codigo, nombre } of statusAccounts) {
+    const select = document.querySelector('#status')
+    let option = document.createElement("option")
+    option.setAttribute("data-tokens", nombre)
+    option.value = codigo
+    option.textContent = nombre
+    select.appendChild(option)
+  }
 }
 
-/*
-    *bancosyvalores/conciliar_tarjetas/get_grupos_tarjetas
-*/
-export const get_cardGroups = tkn => {
-    const url_getCardGroups = process.env.Solu_externo + '/bancosyvalores/conciliar_tarjetas/get_grupos_tarjetas'
-    fetch( url_getCardGroups, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${tkn}`
-        }
-    })
-    .then( resp => resp.json() )
-    .then( resp => {
-        const cardGroups = resp
-        for (const element of cardGroups) {
-            const { id, codigo, nombre } = element
-            // console.log(id, codigo, nombre)
-            const select = document.querySelector('#group')
-            let option = document.createElement("option")
-            option.setAttribute("data-tokens", nombre)
-            option.value = id
-            option.textContent = nombre
-            select.appendChild( option )
-        }
-    })
-    .catch( err => {
-        console.log( err )
-    })
+// conseguir tipo de periodo (Formulario Valores)
+export const get_periodType = async tkn => {
+  const url_getPeriodType = process.env.Solu_externo + '/bancosyvalores/reportes/get_tipos_periodo'
+  const periodTypes = await fetchData(url_getPeriodType, tkn)
+
+	for (const { codigo, nombre } of periodTypes) {
+		const select = document.querySelector('#period-type')
+		let option = document.createElement("option")
+		option.setAttribute("data-tokens", nombre)
+		option.value = codigo
+		option.textContent = nombre
+		select.appendChild( option )
+	}
+}
+
+// Conseguir select order (Formulario Valores)
+export const get_order = async tkn => {
+  const url_getOrder = process.env.Solu_externo + '/bancosyvalores/reportes/get_tipos_orden'
+  const orders = await fetchData(url_getOrder, tkn)
+
+	for (const { codigo, nombre } of orders) {
+		const select = document.querySelector('#order')
+		let option = document.createElement("option")
+		option.setAttribute("data-tokens", nombre)
+		option.value = codigo
+		option.textContent = nombre
+		select.appendChild( option )
+	}
+}
+
+// Conseguir input select valor (Formulario Valores)
+export const get_value = async tkn => {
+  const url_getValue = process.env.Solu_externo + '/bancosyvalores/reportes/get_tipos_valores'
+  const values = await fetchData(url_getValue, tkn)
+
+	for (const { id, nombre } of values) {
+		// console.log(id, nombre)
+		const select = document.querySelector('#value')
+		let option = document.createElement("option")
+		option.setAttribute("data-tokens", nombre)
+		option.value = id
+		option.textContent = nombre
+		select.appendChild( option )
+	}
+}
+
+// Conseguir input select valor (Formulario Valores)
+export const get_cardGroups = async tkn => {
+	const url_getCardGroups = process.env.Solu_externo + '/bancosyvalores/conciliar_tarjetas/get_grupos_tarjetas'
+  const cardGroups = await fetchData(url_getCardGroups, tkn)
+
+	for (const { id, nombre } of cardGroups) {
+		// console.log(id, nombre)
+		const select = document.querySelector('#group')
+		let option = document.createElement("option")
+		option.setAttribute("data-tokens", nombre)
+		option.value = id
+		option.textContent = nombre
+		select.appendChild( option )
+	}
+}
+
+// Conseguir input select cuentas (Formulario Libro Banco)
+export const get_accountsLibroBanco = async tkn => {
+	const url_getAccountsLibroBanco = process.env.Solu_externo + '/bancosyvalores/librobanco/get_cuentas'
+  const {cuentas} = await fetchData(url_getAccountsLibroBanco, tkn)
+
+	for (const { codigo, banco } of cuentas) {
+		// console.log(codigo, banco)
+		const select = document.querySelector('#account')
+		let option = document.createElement("option")
+		option.setAttribute("data-tokens", banco)
+		option.value = codigo
+		option.textContent = banco
+		select.appendChild( option )
+	}
+}
+
+// Conseguir input select operaciones (Formulario Libro Banco)
+export const get_operationsLibroBanco = async tkn => {
+	const url_getOperationsLibroBanco = process.env.Solu_externo + '/bancosyvalores/librobanco/get_operaciones'
+  const {operaciones} = await fetchData(url_getOperationsLibroBanco, tkn)
+
+	for (const { id, operacion } of operaciones) {
+		// console.log(id, operacion)
+		const select = document.querySelector('#operation')
+		let option = document.createElement("option")
+		option.setAttribute("data-tokens", operacion)
+		option.value = id
+		option.textContent = operacion
+		select.appendChild( option )
+	}
 }
